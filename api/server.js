8505ef07b4670cf2147d65e4fd5638a9de38aa83
delimmaemma <calvinhtml@gmail.com>
2023-08-25 23:47:51 -0500

    Fixed server issue

--- api/server.js
+++ api/server.js
@@ -6,7 +6,7 @@ const usersRouter = require('./users/users-router')
 server.use(express.json())
 server.use(logger)
 
-server.use('./api/users', usersRouter)
+server.use('/api/users', usersRouter)
 // global middlewares and the user's router need to be connected here
 
 server.get('/', (req, res) => {
